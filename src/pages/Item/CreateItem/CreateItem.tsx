84def4c5fee8293af29142e3fb38bb2eb7abb486
Kolomiiets Dmytro <nookon@icloud.com>
2024-04-02 12:27:35 +0200

    add aprove and reject and created list with not approved items at create item page

--- src/pages/Item/CreateItem/CreateItem.tsx
+++ src/pages/Item/CreateItem/CreateItem.tsx
@@ -1,10 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {doc, onSnapshot, setDoc} from "firebase/firestore";
+import {doc, onSnapshot, setDoc, Timestamp} from "firebase/firestore";
 import {db} from "../../../firebase";
-import {Autocomplete, Backdrop, Button, CircularProgress, InputAdornment, TextField} from "@mui/material";
+import {
+    Autocomplete, Avatar,
+    Backdrop,
+    Button,
+    Card,
+    CardContent,
+    CircularProgress, Fab,
+    InputAdornment,
+    TextField, Typography
+} from "@mui/material";
 import styles from './CreateItem.module.css'
 import Inputs from "./Inputs";
 import {SnackbarProvider, VariantType, useSnackbar} from 'notistack';
+
+import data from "../../../assets/ItemsInfo.json"
+import {Link} from "react-router-dom";
+import {ITEM_ROUTE} from "../../../utils/consts";
+import dayjs from "dayjs";
+import {useAppSelector} from "../../../hooks/storeHooks";
+import MyButton from "../../../components/MyButton/MyButton";
+import Box from "@mui/material/Box";
+
+import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
+import SwipeLeftIcon from '@mui/icons-material/SwipeLeft';
 
 interface IIndexData {
     oldArray: any[],
@@ -14,6 +34,8 @@ interface IIndexData {
 
 const CreateItem = () => {
     const {enqueueSnackbar} = useSnackbar();
+
+    const {user, error, loading} = useAppSelector(state => state.user)
 
     const [oldData, setOldData] = useState<IIndexData>({
         oldArray: [],
@@ -53,12 +75,27 @@ const CreateItem = () => {
         enqueueSnackbar(title, {variant});
     };
 
+    const addData = async () => {
+        try {
+            const userData = {
+                itemTemplate: [...data]
+            };
+            await setDoc(doc(db, "departments", "PWT70"), userData);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+
     const addTestItem = async () => {
         setIsSending(true);
 
         try {
             const userData = {
-                itemTemplate: [...oldData.oldArray, inputData]
+                itemTemplate: [...oldData.oldArray, {...inputData,
+                    addDate: dayjs().format("YYYY-MM-DD [at] HH:mm"),
+                    addPerson: user.email,
+                    id: Date.now()
+                }]
             };
             await setDoc(doc(db, "departments", "NotApproved"), userData);
             setTimeout(() => {
@@ -67,9 +104,30 @@ const CreateItem = () => {
             }, 250)
         } catch (e) {
             console.error("Error adding document: ", e);
-            handleClickVariant('success', e);
+            handleClickVariant('error', e);
         } finally {
             setIsSending(false)
+        }
+    }
+
+    const onRejectClick = async (id: number) => {
+        const filter = oldData.oldArray.filter(item => item.id !== id)
+        setIsSending(true)
+
+        try {
+            await setDoc(doc(db, "departments", "NotApproved"), {
+                itemTemplate: [...filter],
+                lastUpdate: dayjs().format("YYYY-MM-DD [at] HH:mm"),
+                updateBy: user.email
+            });
+
+            setTimeout(() => {
+                handleClickVariant('success', "Item was success delete");
+                setIsSending(false);
+            })
+        } catch (error) {
+            handleClickVariant('error', error);
+            setIsSending(false);
         }
     }
 
@@ -78,11 +136,50 @@ const CreateItem = () => {
             <h4>ADD NEW ITEM</h4>
             <hr/>
             <article style={{color: "gray"}}>Usually an employee cannot add a new item immediately, but we will review your item and approve it if it is correct.</article>
-            <Backdrop style={{zIndex: 2}} open={isSending}>
+            <Backdrop style={{zIndex: 99}} open={isSending}>
                 <CircularProgress color="inherit"/>
             </Backdrop>
             <Inputs inputData={inputData} setInputData={setInputData}/>
             <Button onClick={addTestItem} fullWidth={true} variant="outlined">Send</Button>
+            {/*<Button onClick={addData} fullWidth={true} variant="outlined">Add data</Button>*/}
+
+            <h5 style={{marginTop: 24}}>Not approved items</h5>
+            <hr/>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 4
+            }}>
+                {oldData.oldArray.map((el, index) => (
+                    <Card key={index} sx={{ minWidth: 240 }} variant={"outlined"} raised={true}>
+                        <CardContent style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                            <div>
+                                <Typography color="text.secondary" variant={"subtitle1"}>
+                                    <Link>{el.myIndex} | {el.type}</Link>
+                                </Typography>
+                                <Typography fontSize={16} color="text.secondary" variant={"subtitle1"}>
+                                    <article>{el.description}</article>
+                                </Typography>
+                                <Typography fontSize={16} color="text.secondary" variant={"subtitle1"}>
+                                    {el.addDate} | {el.addPerson}
+                                </Typography>
+                            </div>
+                            <div>
+                                <Box style={{display: "flex", flexDirection: "column", gap: 14}} fontSize={12}>
+                                    <Fab variant="extended" size={"medium"} color={"success"}>
+                                        <LibraryAddCheckIcon sx={{ mr: 1 }} />
+                                        Approve
+                                    </Fab>
+                                    <Fab onClick={() => onRejectClick(el.id)} variant="extended" size={"medium"} color={"error"}>
+                                        <SwipeLeftIcon sx={{ mr: 1 }} />
+                                        Reject
+                                    </Fab>
+                                </Box>
+                            </div>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
         </div>
     );
 };
